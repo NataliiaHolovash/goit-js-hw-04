78e76369794ba600cb77a25cf798f307a4ae300f
NataliiaHolovash <n.golovash@ukr.net>
2024-03-21 00:02:48 +0100

    save1

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,16 @@
 'use strict';
 
 function calcAverageCalories(days) {
+    if (days.length === 0) {
+        return 0; 
+    }
+    
     let totalCalories = 0;
      
     for (const day of days) {
         totalCalories += day.calories; 
     }  
-    return totalCalories / days.length;
+    return totalCalories / days.length; 
 }
 
 console.log(
